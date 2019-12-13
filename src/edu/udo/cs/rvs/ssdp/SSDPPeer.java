8f6ebf13534c2e048456457e21b9e46e73ac72f4
Sopr051 <sopr051>
2019-12-13 10:15:24 +0100

    init all files

---
+++ src/edu/udo/cs/rvs/ssdp/SSDPPeer.java
@@ -0,0 +1,14 @@
+package edu.udo.cs.rvs.ssdp;
+
+/**
+ * This class is first instantiated on program launch and IF (and only if) it
+ * implements Runnable, a {@link Thread} is created and started.
+ *
+ */
+public class SSDPPeer
+{
+	public SSDPPeer()
+	{
+
+	}
+}
